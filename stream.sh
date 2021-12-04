553a3988121bc726ce84d21e1745c6a41cf6f95c
Christian Semmler <mail@csemmler.com>
2021-12-04 15:29:35 +0100

    improve ffmpeg command

--- stream.sh
+++ stream.sh
@@ -4,9 +4,11 @@ set -e
 
 while true
 do
-  ffmpeg -loglevel error -y -re \
+  ffmpeg -loglevel info -y -re \
     -f image2 -loop 1 -i bg.png \
     -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
-    -codec:a aac -b:a 128k -ar 44100 -maxrate 5000k -bufsize 2500k -shortest -strict experimental -q:v 5 \
+    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
+    -c:a aac -b:a 128k -ar 44100 \
     -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
 done
